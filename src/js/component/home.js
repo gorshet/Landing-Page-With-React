7e708da3b785916d46293b2316b76a462272f518
gorshet <jorgemontes771@gmail.com>
2019-07-26 00:54:48 +0000

    New Card

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Card } from "./card.js";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
@@ -8,7 +8,7 @@ export class Home extends React.Component {
 	render() {
 		return (
 			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
+				<Card />
 			</div>
 		);
 	}
