a308f70ba97190d138687819c5f2b1adfe6dd9aa
gorshet <jorgemontes771@gmail.com>
2019-07-27 14:20:19 +0000

    advances

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -3,17 +3,76 @@ import React from "react";
 //create your first component
 export const Card = () => {
 	return (
-		<div className="card">
-			<img src="..." className="card-img-top" alt="..." />
-			<div className="card-body">
-				<h5 className="card-title">Card title</h5>
-				<p className="card-text">
-					Some quick example text to build on the card title and make
-					up the bulk of the cards content.
-				</p>
-				<a href="#" className="btn btn-primary">
-					Go somewhere
-				</a>
+		<div className="container">
+			<div class="card-deck">
+				<div className="card">
+					<img
+						src="https://i.ytimg.com/vi/c_qFR0sUECQ/maxresdefault.jpg"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing
+							elit. Sapiente esse necessitatibus neque.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+					</div>
+				</div>
+				<div className="card">
+					<img
+						src="https://i.ytimg.com/vi/c_qFR0sUECQ/maxresdefault.jpg"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing
+							elit. Sapiente esse necessitatibus neque.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+					</div>
+				</div>
+				<div className="card">
+					<img
+						src="https://i.ytimg.com/vi/c_qFR0sUECQ/maxresdefault.jpg"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing
+							elit. Sapiente esse necessitatibus neque.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+					</div>
+				</div>
+				<div className="card">
+					<img
+						src="https://i.ytimg.com/vi/c_qFR0sUECQ/maxresdefault.jpg"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing
+							elit. Sapiente esse necessitatibus neque.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { Card } from "./card.js";
+import { Card } from "./card";
+import { Jumbotron } from "./jumbotron";
 import { Navbar } from "./navbar";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 export class Home extends React.Component {
 	render() {
 		return (
-			<div className="text-center mt-5">
-				<Card />
+			<React.Fragment>
 				<Navbar />
-				<h1>Hello Rigo!</h1>
-			</div>
+				<Jumbotron />
+				<Card />
+			</React.Fragment>
 		);
 	}
 }
